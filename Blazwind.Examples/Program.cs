3d27a13a52186ef5ed3ccb3d62cf8914c709e9b8
Cihad GUNDOGDU <cihadgundogdu@gmail.com>
2026-01-19 17:33:38 +0300

    feat: Migrate Blazwind.Examples to Blazor WebAssembly and add a GitHub Pages deployment workflow.

--- Blazwind.Examples/Program.cs
+++ Blazwind.Examples/Program.cs
@@ -1,38 +1,15 @@
 using Blazwind.Examples.Components;
 using Blazwind.Components;
-using Blazwind.Components.Dialog;
-using Blazwind.Components.Drawer;
-using Blazwind.Components.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-// Blazwind Services
 builder.Services.AddBlazwind();
 
-// HttpClient for loading static assets (GeoJSON, etc.)
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5211/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
-
-app.UseAntiforgery();
-
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
+await builder.Build().RunAsync();
